8df71505f0b6c76336e874761f9d15f1b5f5427e
石磊 <383821949@qq.com>
2017-07-28 12:14:44 +0800

    add *

---
+++ jinja2.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from jinja2 import Template
+import os
+
+def index():
+    with open("template.json") as fd:
+        result = fd.read()
+
+    template = Template(result)
+    data = template.render(MetadataName=os.getenv('MetadataName'),
+                           MetadataNamespace=os.getenv('MetadataNamespace'),
+                           MetadataVersion=os.getenv('MetadataVersion'),
+                           SpecReplicas=os.getenv('SpecReplicas'),
+                           SpecTemplateMetadataLabelsVersi=os.getenv('SpecTemplateMetadataLabelsVersion'),
+                           ContainerImage=os.getenv('ContainerImage'),
+                           ServicesName=os.getenv('ServicesName'),
+                           AppName=os.getenv('AppName'),
+                           CPU=os.getenv('CPU'),
+                           MEM=os.getenv('MEM'),
+                           PortsName=os.getenv('PortsName'),
+                           PortsEndpoint=os.getenv('PortsEndpoint'),
+                           volumeMountsName=os.getenv('volumeMountsName'),
+                           volumeMountsEndpoint=os.getenv('volumeMountsEndpoint'),
+                           volumesName=os.getenv('volumesName'),
+                           volumesEndpoint=os.getenv('volumesEndpoint')
+                            )
+    print(data)
+
+index()
+
